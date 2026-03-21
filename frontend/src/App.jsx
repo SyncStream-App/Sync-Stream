b3774cd500e2bc8c2fcc337fda7385302abc79f4
KaranG777 <gattanikaran078@gmail.com>
2026-03-21 21:13:32 +0000

    SYNC-86:Finalcodefix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ export default function App(){
   }, [theme])
 
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path ='/login' element={<LoginPage />} />
         <Route path ='/onboarding' element={<OnboardingPage />} />
